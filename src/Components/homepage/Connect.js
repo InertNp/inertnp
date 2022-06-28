8cfb35a2eadae52b2c0c832296839683db1c2551
inertnp <sabhi1757@gmail.com>
2022-06-29 00:11:35 +0545

    some changes with gmail

--- src/Components/homepage/Connect.js
+++ src/Components/homepage/Connect.js
@@ -11,36 +11,24 @@ export function Connect() {
   return (
     <Container className="w-11/12 bg-gray-100">
       <SubContainer className={`shadow-md`}>
-        <div className={`flex  h-full justify-center items-center`}>
+        <div className={`flex  h-full justify-center items-center flex-col`}>
+         
           <h1 className={`text-2xl font-bold pl-3`}>Connect With Me:</h1>
+          <div>
+            <p className="text-3xl">sabhi1757@gmail.com</p>
+          </div>
           <h2 className={`p-5 `}>
             <a href={aboutme.github} className={`pl-3 hover:text-sky-500`}>
-              <FontAwesomeIcon
-                icon={faGithub}
-                size={`5x`}
-             
-              /> 
+              <FontAwesomeIcon icon={faGithub} size={`5x`} />
             </a>
             <a href={aboutme.linkedin} className={`pl-3 hover:text-sky-500`}>
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                size={`5x`}
-           
-              />
+              <FontAwesomeIcon icon={faLinkedin} size={`5x`} />
             </a>
             <a href={aboutme.twitter} className={`pl-3 hover:text-sky-500`}>
-              <FontAwesomeIcon
-                icon={faTwitter}
-                size={`5x`}
-             
-              />
+              <FontAwesomeIcon icon={faTwitter} size={`5x`} />
             </a>
             <a href={`/`} className={`pl-3 hover:text-sky-500`}>
-              <FontAwesomeIcon
-                icon={faDiscord}
-                size={`5x`}
-             
-              />
+              <FontAwesomeIcon icon={faDiscord} size={`5x`} />
             </a>
           </h2>
         </div>
